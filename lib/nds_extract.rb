231a87a90fa7afe771f85c1a9ce681e1ea1de0bb
Ayako Wiltshire <awittyl44@gmail.com>
2019-11-13 16:34:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,9 +7,7 @@ row_index = 0
 gross = {}
 while row_index < nds.length do
 directors_name = nds[row_index][:name]
-gross = {
-  directors_name => 0 
-}
+gross[directors_name] = {}
   # The directors_name is a variable of all the directors in the nds.
   # directors_name.each do |:name |
     
@@ -25,28 +23,7 @@ gross = {
  element_index += 1 
  end
  
- 
- 
- 
-# directors_info = directors_name, movies
-# movies[0..7].each do |movie|
-# pp movie   
-# end
-#This process creates a variable to access all of the movies from a hash.
-
- # directors_info is a variable containing all information of the first 2 variables.
-
-## directors_gross = movies[column_index][:worldwide_gross]
-# # The directors_gross is a variable of the sum of all of each directors movies.
-
-# totals = {directors_name => directors_gross}
-# totals is a hash combining the two varaibles.
-
   row_index += 1
-  # pp directors_info[0...7]
-  pp 
-  # pp directors_name, movies
-# pp directors_info
 end
 
 
