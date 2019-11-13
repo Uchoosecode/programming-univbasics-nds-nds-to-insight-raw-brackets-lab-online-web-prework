293c9a409169aec3f1166609ddf386b44f24f32b
Ayako Wiltshire <awittyl44@gmail.com>
2019-11-13 16:40:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,17 +7,14 @@ row_index = 0
 gross = {}
 while row_index < nds.length do
 directors_name = nds[row_index][:name]
-gross[directors_name] = 0 
   # The directors_name is a variable of all the directors in the nds.
-  # directors_name.each do |:name |
-    
-  # end
+gross[directors_name] = 0 
+# This takes the variable directors_name, adds it into the gross hash and gives it a value of 0 as a counter.
  movies = nds[row_index][:movies]
   # The movies variable show the names of all the movies.
  element_index = 0 
  
  while element_index < movies.length
-# binding.pry 
  gross[directors_name] += movies[element_index][:worldwide_gross]
  
  element_index += 1 
@@ -26,22 +23,5 @@ gross[directors_name] = 0
   row_index += 1
 end
 
-
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
 pp gross
 end
